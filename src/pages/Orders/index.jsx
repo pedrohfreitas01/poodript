e41e467dcf0da3c93e09252fea83f4897c31b431
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-07-16 09:44:11 -0300

    🐛 Fix trash  SVG styling bug on OrderPage

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
-import { Container, Image,Section, Div, Svg } from './style';
+import { Container, Image, Section, Div, TrashContainer } from "./style";
 import orderBag from "../../assets/orderBag.png";
 import ContainerItens from '../../components/ContainerItens';
 import { H1 } from "../../components/Title/style";
@@ -15,14 +15,12 @@ function Orders() {
       <ContainerItens>
         <H1>Orders in Progress</H1>
         <Section>
-          <Div>
-              ola gente
-          </Div>
-          <Svg><img src={trash} alt="delete"/></Svg>
+          <Div>ola gente</Div>
+          <TrashContainer/>
         </Section>
       </ContainerItens>
     </Container>
-  )
+  );
 }
 
 export default Orders
--- src/pages/Orders/style.jsx
+++ src/pages/Orders/style.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import trash from "../../assets/trash.svg";
+
 
 export const Container = styled.div`
     display: flex;
@@ -15,22 +17,26 @@ export const Image = styled.img`
 
 export const Section = styled.section`
   display: flex;
-
-
   border-radius: 14px;
   background: rgba(255, 255, 255, 0.25);
   width: 342px;
   height: 101px;
   flex-shrink: 0;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Div = styled.div`
-  
+  background: none;
 `
 
-export const Svg = styled.svg`
-  width: 24px;
-  height: 28px;
-  flex-shrink: 0;
-  display: flex;
+export const TrashContainer = styled.div`
+  width: 19.2px;
+  height: 22.4px;
+  background: url(${trash}) no-repeat center;
+  background-size: contain;
+
 `;
